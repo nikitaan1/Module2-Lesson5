3d09c2a44606324e69467481584d695267461a75
nikitaan1 <ancevskinikita@gmail.com>
2024-04-02 09:09:47 -0400

    task 1

--- Task_1_The_Calculator_App.py
+++ Task_1_The_Calculator_App.py
@@ -1,16 +1,40 @@
-def multiply(a, b):
-    result = a * b
-    return result
+def multiplication(a, b):
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    return a * b
 
-def divide(a, b):
-    result = a / b 
-    return result
+def division(a, b):
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    return a / b
 
-def plus(a, b):
-    result = a + b 
-    return result
+def addition(a, b):
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))    
+    return a + b
 
-def substract(a, b):
-    result = a - b
-    return result
+def subtraction(a, b):
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    return a - b
 
+#Task 2
+
+print("Welcome:")
+print("1. multiplication")
+print("2. division")
+print("3. addition")
+print("3. subtraction")
+
+choice = int(input("Select your desired operation "))
+
+if choice == 1:
+    multiplication()
+elif choice == 2:
+    division()
+elif choice == 3:
+    addition()
+elif choice == 4:
+    subtraction()
+else:
+    print("Invalid choice")
--- Task_4_The_Quiz_Game.py
+++ Task_4_The_Quiz_Game.py
@@ -23,6 +23,27 @@ answers = ["Blue", "10", "Earth"]
 
 quiz(questions_list, answers)
 
+#Task3
 
+def quiz(questions, answers):
+    total_questions = len(questions)
+    score = 0
+
+    for i in range(total_questions):
+        user_answer = input(questions[i] + " ")
+
+        if user_answer.lower() == answers[i].lower():
+            print("Correct!")
+            score += 1
+        else:
+            print(f"Wrong! The correct answer is: {answers[i]}")
+
+    
+    print(f"You got {score} out of {total_questions} questions correct.")
+
+questions_list = ["What color is the sky?", "Calculate: 5 + 5", "On which planet do we live?"]
+answers = ["Blue", "10", "Earth"]
+
+quiz(questions_list, answers)
 
     
